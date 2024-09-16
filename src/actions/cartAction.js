78c777df79994b67d75a96dee362a937cb7c60b7
Yogesh Suhas Naikwadi <yogeshnaikwadi2000@gmail.com>
2024-09-16 19:18:04 +0530

    eight

--- src/actions/cartAction.js
+++ src/actions/cartAction.js
@@ -4,7 +4,7 @@ import { ADD_TO_CART, REMOVE_CART_ITEM, SAVE_SHIPPING_INFO } from "../constants/
 
 export const addItemsToCart = (id,quantity) => async(dispatch,getState) => {
 
-    const {data} = await axios.get(`/api/v1/product/${id}`);
+    const {data} = await axios.get(`https://ecommerce-website-nuyo.onrender.com/api/v1/product/${id}`);
 
     dispatch({
         type:ADD_TO_CART,
--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -12,7 +12,7 @@ export const login = (email,password) => async(dispatch) =>{
 
 
         const {data} =await axios.post(
-            `/api/v1/login`,
+            `https://ecommerce-website-nuyo.onrender.com/api/v1/login`,
             {email,password},
             config
         )
@@ -41,7 +41,7 @@ export const register = (userData) => async(dispatch) => {
         const config= { headers : { "Content-Type":"multipart/form-data" }}
 
         const { data } = await axios.post(
-            `/api/v1/register`,
+            `https://ecommerce-website-nuyo.onrender.com/api/v1/register`,
             userData,
             config
         )
@@ -71,7 +71,7 @@ export const loadUser = () => async(dispatch) =>{
        
 
 
-        const {data} =await axios.get(`/api/v1/me`);
+        const {data} =await axios.get(`https://ecommerce-website-nuyo.onrender.com/api/v1/me`);
 
         dispatch({
             type:LOAD_USER_SUCCESS,
@@ -96,7 +96,7 @@ export const logout = () => async(dispatch) =>{
     try{
        
 
-       await axios.get(`/api/v1/logout`);
+       await axios.get(`https://ecommerce-website-nuyo.onrender.com/api/v1/logout`);
 
         dispatch({
             type:LOGOUT_USER_SUCCESS
@@ -120,7 +120,7 @@ export const updateProfile = (userData) => async (dispatch) => {
 
     const config = { headers: { "Content-Type": "multipart/form-data" } };
 
-    const { data } = await axios.put(`/api/v1/me/update`, userData, config);
+    const { data } = await axios.put(`https://ecommerce-website-nuyo.onrender.com/api/v1/me/update`, userData, config);
 
     dispatch({ type: UPDATE_PROFILE_SUCCESS, payload: data.success });
   } catch (error) {
@@ -139,7 +139,7 @@ export const updatePassword = (passwords) => async (dispatch) => {
       const config = { headers: { "Content-Type": "application/json" } };
   
       const { data } = await axios.put(
-        `/api/v1/password/update`,
+        `https://ecommerce-website-nuyo.onrender.com/api/v1/password/update`,
         passwords,
         config
       );
@@ -159,7 +159,7 @@ export const updatePassword = (passwords) => async (dispatch) => {
   
       const config = { headers: { "Content-Type": "application/json" } };
   
-      const { data } = await axios.post(`/api/v1/password/forgot`, email, config);
+      const { data } = await axios.post(`https://ecommerce-website-nuyo.onrender.com/api/v1/password/forgot`, email, config);
   
       dispatch({ type: FORGOT_PASSWORD_SUCCESS, payload: data.message });
     } catch (error) {
@@ -177,7 +177,7 @@ export const updatePassword = (passwords) => async (dispatch) => {
       const config = { headers: { "Content-Type": "application/json" } };
   
       const { data } = await axios.put(
-        `/api/v1/password/reset/${token}`,
+        `https://ecommerce-website-nuyo.onrender.com/api/v1/password/reset/${token}`,
         passwords,
         config
       );
@@ -195,7 +195,7 @@ export const updatePassword = (passwords) => async (dispatch) => {
   export const getAllUsers = () => async (dispatch) => {
     try {
       dispatch({ type: ALL_USERS_REQUEST });
-      const { data } = await axios.get(`/api/v1/admin/users`);
+      const { data } = await axios.get(`https://ecommerce-website-nuyo.onrender.com/api/v1/admin/users`);
   
       dispatch({ type: ALL_USERS_SUCCESS, payload: data.users });
     } catch (error) {
@@ -210,7 +210,7 @@ export const updatePassword = (passwords) => async (dispatch) => {
     try {
       dispatch({ type: DELETE_USER_REQUEST });
   
-      const { data } = await axios.delete(`/api/v1/admin/user/${id}`);
+      const { data } = await axios.delete(`https://ecommerce-website-nuyo.onrender.com/api/v1/admin/user/${id}`);
   
       dispatch({ type: DELETE_USER_SUCCESS, payload: data });
     } catch (error) {
@@ -227,7 +227,7 @@ export const updatePassword = (passwords) => async (dispatch) => {
 export const getUserDetails = (id) => async (dispatch) => {
   try {
     dispatch({ type: USER_DETAILS_REQUEST });
-    const { data } = await axios.get(`/api/v1/admin/user/${id}`);
+    const { data } = await axios.get(`https://ecommerce-website-nuyo.onrender.com/api/v1/admin/user/${id}`);
 
     dispatch({ type: USER_DETAILS_SUCCESS, payload: data.user });
   } catch (error) {
@@ -243,7 +243,7 @@ export const updateUser = (id, userData) => async (dispatch) => {
     const config = { headers: { "Content-Type": "application/json" } };
 
     const { data } = await axios.put(
-      `/api/v1/admin/user/${id}`,
+      `https://ecommerce-website-nuyo.onrender.com/api/v1/admin/user/${id}`,
       userData,
       config
     );
