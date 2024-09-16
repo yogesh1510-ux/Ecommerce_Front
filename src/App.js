d3f17d0d5992d7750b973eff960d92e2ddbd7144
Yogesh Suhas Naikwadi <yogeshnaikwadi2000@gmail.com>
2024-09-16 19:23:23 +0530

    teb

--- src/App.js
+++ src/App.js
@@ -73,7 +73,7 @@ function App() {
   return (
     <Router>
        <Header />
-      {/* {isAuthenticated && <UserOptions user={user} />} */}
+      {isAuthenticated && <UserOptions user={user} />}
 
    
 
